5b4cf67a5e35d6eaecbf61dcd15e3460f4d2435e
Hayden Wagner <haydenblainewagner@gmail.com>
2017-01-25 18:55:18 -0500

    Added another post

--- src/app/post.ts
+++ src/app/post.ts
@@ -1,9 +1,11 @@
 export class Post{
   constructor(
     public id: string,
+    public cardImage: string,
     public title: string,
     public author: string,
     public preview: string,
     public pubDate: string,
+    public mainPoints: string,
     public markdown: string) { }
 }
