fc6c07ea7cab208c1598d747c3ee7647f9c7e653
Hayden Wagner <haydenblainewagner@gmail.com>
2017-02-17 21:23:07 -0500

    Adds footer links, corrects title and adds sample favicon

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -6,13 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
-  copyright = `All code licensed
-      <a rel="license" href="https://github.com/twbs/bootstrap/blob/master/LICENSE" target="_blank">MIT</a>, site content 
-      <a rel="license" href="https://creativecommons.org/licenses/by/3.0/" target="_blank">CC BY 3.0</a>`;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  footerData = {
+    "copyrightLinkContent": {
+      "mit":"rel='license' href='https://github.com/twbs/bootstrap/blob/master/LICENSE' target='_blank'",
+      'cc3':'rel="license" href="https://creativecommons.org/licenses/by/3.0/" target="_blank"'
+    },
+    "buildNoteLinkContent": {
+      "ng2":"rel='angular link' href='https://angular.io/' target='_blank'",
+      'heroku':'rel="heroku link" href="https://www.heroku.com/" target="_blank"',
+      'repo':'rel="site repo link" href="https://github.com/haydenwagner/oscar-goldman" target="_blank"'
+    }
+  }
+
+  buildNote = `Site built with the <a ${this.footerData.buildNoteLinkContent.ng2}>Angular 2</a> web framework&mdash;check 
+              out the source code on <a ${this.footerData.buildNoteLinkContent.repo}>Github</a>.
+              All code licensed <a ${this.footerData.copyrightLinkContent.mit}>MIT</a>, site content <a ${this.footerData.copyrightLinkContent.cc3}>CC BY 3.0</a>`;
 }
