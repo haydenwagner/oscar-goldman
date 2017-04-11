bfcfdb168666d06d6e51050ce5a298d0d160a67c
Hayden Wagner <haydenblainewagner@gmail.com>
2017-04-11 01:11:58 -0400

    Fix server path to dist index file

--- server.js
+++ server.js
@@ -11,11 +11,11 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use( cors() );
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static('./dist'));
 app.use('/api', api);
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/dist/index.html'));
+  res.sendFile('./dist/index.html');
 });
 
 const port = process.env.PORT || '3000';
